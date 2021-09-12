ee29090f38aaf49c8ef72ca5b72313ddf3fb0814
claudiovalen <61560556+claudiovalen@users.noreply.github.com>
2021-09-12 17:04:42 +0200

    Add files via upload

--- ProgettoIcon-main/Wine Project/main.py
+++ ProgettoIcon-main/Wine Project/main.py
@@ -1,7 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sb
 from sklearn.metrics import plot_confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
@@ -10,27 +8,18 @@ from sklearn.metrics import classification_report
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 
-df = pd.read_csv('dataset/wine.csv')
+df = pd.read_csv('../dataset/wine.csv')
 
+print(df.head())
 
 df = df.dropna()
-boolean = False
-boolean = df.isnull().values.any()
-print(boolean)
 
 Y = df['quality']
-X = df.drop('quality',axis=1)
-
-print(Y.value_counts())
-plt.figure()
-plt.tick_params(axis='x', which='major')
-sb.countplot(x= 'quality', data=df)
-plt.show()
-
+X = df.drop(columns=['quality','pH','residual sugar','density'],axis=1)
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=0)
 
-
+# Standardize
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
@@ -38,30 +27,21 @@ X_test = sc.transform(X_test)
 #Create a function within many Machine Learning Models
 def models(X_train,Y_train,X_test,Y_test):
 
-    #Using KNeighborsClassifier Method of neighbors class to use Nearest Neighbor algorithm
-    knn= KNeighborsClassifier(p=1)
+    #Using the Nearest Neighbor algorithm
+    knn = KNeighborsClassifier(p=1)
     knn.fit(X_train, Y_train)
     knn_pred = knn.predict(X_test)
     knn_classification = classification_report(Y_test,knn_pred)
 
-    #Using RandomForestClassifier method of ensemble class to use Random Forest Classification algorithm
+    #Using the Random Forest Classification algorithm
     forest = RandomForestClassifier(max_depth=11,random_state=10)
     forest.fit(X_train, Y_train)
     forest_pred = forest.predict(X_test)
     forest_classification = classification_report(Y_test,forest_pred)
 
-    print('[1]K Nearest Neighbor Test Accuracy:')
+    print('[0]K Nearest Neighbor Test Accuracy:')
     print(knn_classification)
-    print('[6]Random Forest Classifier Test Accuracy:')
+    print('[1]Random Forest Classifier Test Accuracy:')
     print(forest_classification)
 
     return knn,forest
-
-
-
-
-
-
-
-
-
